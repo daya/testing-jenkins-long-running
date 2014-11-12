28a703ef03e3216c3551fd973c299ac99f60211e
Daya Sharma <daya.sharma+github@gmail.com>
2014-11-12 17:46:34 -0600

    bin sh simple

--- sleep.sh
+++ sleep.sh
@@ -13,3 +13,6 @@ echo ">>>>>>> 4 for 3 seconds\n";
 sleep 3;
 echo "woke up 4 after 3 seconds `date '+%Y/%m/%d %H:%M:%S'`";
 echo "sleeping for 150 seconds"; sleep 150; echo "woke up after 150 seconds"
+echo ">>>>>>> 5 for 3 seconds\n";
+sleep 3;
+echo "woke up 5 after 3 seconds `date '+%Y/%m/%d %H:%M:%S'`";
