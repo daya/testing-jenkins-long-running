0a27563cedb06e6b1cd14858abd909bc9a0bf60a
Daya Sharma <daya.sharma+github@gmail.com>
2014-11-12 17:11:59 -0600

    extra echo

--- sleep.sh
+++ sleep.sh
@@ -1,1 +1,7 @@
+for i in {1..4..1}
+do
+  echo ">>>>>>> sleeping for 3 seconds\n"
+  sleep 3
+  echo "woke after 3 seconds"
+done
 echo "sleeping for 150 seconds"; sleep 150; echo "woke up after 150 seconds"
