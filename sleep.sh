ae4a5ebe42f44c3a98973c77626c3f0971a3a89f
Daya Sharma <daya.sharma+github@gmail.com>
2014-11-12 17:16:21 -0600

    extra echo

--- sleep.sh
+++ sleep.sh
@@ -2,6 +2,6 @@ for i in {1..4..1}
 do
   echo ">>>>>>> sleeping for 3 seconds\n"
   sleep 3
-  echo "woke after 3 seconds"
+  echo "woke after 3 seconds `date '+%Y/%m/%d %H:%M:%S'`"
 done
 echo "sleeping for 150 seconds"; sleep 150; echo "woke up after 150 seconds"
