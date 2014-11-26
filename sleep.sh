9358a9032226a6059b1c4f24e823e8ac99f7f85f
Daya Sharma <daya.sharma+github@gmail.com>
2014-11-26 16:59:16 -0600

    just sleeping for 5 sec

--- sleep.sh
+++ sleep.sh
@@ -1,18 +1,18 @@
 #/bin/sh
 
-echo ">>>>>>> 1 for 3 seconds\n";
-sleep 3;
-echo "woke up 1 after 3 seconds `date '+%Y/%m/%d %H:%M:%S'`";
-echo ">>>>>>> 2 for 3 seconds\n";
-sleep 3;
-echo "woke up 2 after 3 seconds `date '+%Y/%m/%d %H:%M:%S'`";
-echo ">>>>>>> 3 for 3 seconds\n";
-sleep 3;
-echo "woke up 3 after 3 seconds `date '+%Y/%m/%d %H:%M:%S'`";
-echo ">>>>>>> 4 for 3 seconds\n";
-sleep 3;
-echo "woke up 4 after 3 seconds `date '+%Y/%m/%d %H:%M:%S'`";
-echo "sleeping for 150 seconds"; sleep 150; echo "woke up after 150 seconds"
-echo ">>>>>>> 5 for 3 seconds\n";
-sleep 3;
-echo "woke up 5 after 3 seconds `date '+%Y/%m/%d %H:%M:%S'`";
+# echo ">>>>>>> 1 for 3 seconds\n";
+# sleep 3;
+# echo "woke up 1 after 3 seconds `date '+%Y/%m/%d %H:%M:%S'`";
+# echo ">>>>>>> 2 for 3 seconds\n";
+# sleep 3;
+# echo "woke up 2 after 3 seconds `date '+%Y/%m/%d %H:%M:%S'`";
+# echo ">>>>>>> 3 for 3 seconds\n";
+# sleep 3;
+# echo "woke up 3 after 3 seconds `date '+%Y/%m/%d %H:%M:%S'`";
+# echo ">>>>>>> 4 for 3 seconds\n";
+# sleep 3;
+# echo "woke up 4 after 3 seconds `date '+%Y/%m/%d %H:%M:%S'`";
+echo "sleeping for 5 seconds"; sleep 5; echo "woke up after 5 seconds"
+# echo ">>>>>>> 5 for 3 seconds\n";
+# sleep 3;
+# echo "woke up 5 after 3 seconds `date '+%Y/%m/%d %H:%M:%S'`";
