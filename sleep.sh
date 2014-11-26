ced124687eb3f6a1ccad26da4e5e9b063d5ab3bf
Daya Sharma <daya.sharma+github@gmail.com>
2014-11-26 17:48:11 -0600

    just sleeping for 70 sec

--- sleep.sh
+++ sleep.sh
@@ -12,7 +12,7 @@
 # echo ">>>>>>> 4 for 3 seconds\n";
 # sleep 3;
 # echo "woke up 4 after 3 seconds `date '+%Y/%m/%d %H:%M:%S'`";
-echo "sleeping for 5 seconds"; sleep 5; echo "woke up after 5 seconds"
+echo "sleeping for 70 seconds"; sleep 70; echo "woke up after 5 seconds"
 # echo ">>>>>>> 5 for 3 seconds\n";
 # sleep 3;
 # echo "woke up 5 after 3 seconds `date '+%Y/%m/%d %H:%M:%S'`";
